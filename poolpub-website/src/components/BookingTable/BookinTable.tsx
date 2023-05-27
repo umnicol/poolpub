06dd3dbee2c236d52911a1528186d03cb92f8fa5
luciasupova <luciasupovaa@gmail.com>
2023-05-27 14:45:29 +0200

    delete booking

--- poolpub-website/src/components/BookingTable/BookinTable.tsx
+++ poolpub-website/src/components/BookingTable/BookinTable.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { db, auth } from "<poolpub>/firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, deleteDoc, doc } from "firebase/firestore";
 import Button from "../Button/Button";
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri'; // Import the delete icon component from React Icons
 import styles from "./BookingTable.module.css";
-
 
 export default function BookingTable() {
   const [bookings, setBookings] = useState<Booking[]>([]);
+  const [selectedBooking, setSelectedBooking] = useState<Booking | null>(null);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   interface Booking {
     id: string;
@@ -52,17 +54,44 @@ export default function BookingTable() {
     loadBookings();
   }, []);
 
+  const deleteBooking = async (bookingId: string) => {
+    setSelectedBooking(bookings.find((booking) => booking.id === bookingId) || null);
+    setShowConfirmation(true);
+  };
+
+  const confirmDeleteBooking = async () => {
+    if (selectedBooking) {
+      try {
+        await deleteDoc(doc(db, "bookings", selectedBooking.id));
+        setBookings(bookings.filter((booking) => booking.id !== selectedBooking.id));
+        console.log("Booking deleted successfully!");
+      } catch (error) {
+        console.error("Error deleting booking:", error);
+      }
+    }
+    setShowConfirmation(false);
+    setSelectedBooking(null);
+  };
+
+  const cancelDeleteBooking = () => {
+    setShowConfirmation(false);
+    setSelectedBooking(null);
+  };
 
   const buttonBooking = () => {
-    window.location.href = '/booking';
+    window.location.href = "/booking";
   };
-  
+
   return (
     <div>
-      <h2>My Bookings</h2>
-      <div className={styles.bookingButton}>
-      <Button label={'+add new'} onClick={buttonBooking} />
+      <div className={styles.headingContainer}>
+        <div className={styles.title}>
+          <h2>My Bookings</h2>
         </div>
+        <div className={styles.bookingButton}>
+          <Button label="+add new" onClick={buttonBooking} />
+        </div>
+      </div>
       <table className={styles.bookingTable}>
         <thead>
           <tr>
@@ -74,6 +103,7 @@ export default function BookingTable() {
             <th>Time</th>
             <th>People</th>
             <th>Special Requirements</th>
+            <th>Edit/Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -87,10 +117,23 @@ export default function BookingTable() {
               <td>{booking.time}</td>
               <td>{booking.people}</td>
               <td>{booking.message}</td>
+              <td className={styles.deleteIcon}>
+                <RiDeleteBinLine onClick={() => deleteBooking(booking.id)} />
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      
+      {showConfirmation && (
+        <div className={styles.confirmationDialog}>
+          <h3>Are you sure you want to delete your booking?</h3>
+          <div>
+            <button onClick={confirmDeleteBooking}>Yes</button>
+            <button onClick={cancelDeleteBooking}>No</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
